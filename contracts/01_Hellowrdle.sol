93e9832a463d2b3da0855199cc5749a2dc32fa6f
Albert Garuda <algarud@gmail.com>
2022-01-31 15:09:07 +0200

    Hellowrdle

---
+++ contracts/01_Hellowrdle.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWrdle {
+
+    string number;
+
+    constructor() {
+        number = "hellowrdle";
+    }
+
+    function retrieve() public view returns (string memory){
+        return number;
+    }
+}
