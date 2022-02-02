6e3aa1a5142967533d29a9a7a9f6d61d6bb6abe0
Albert Garuda <algarud@gmail.com>
2022-02-02 23:07:49 +0200

    Having provlem with ganache provider in test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,25 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-const web3 = new Web3(ganache.provider);
+//const web3 = new Web3(ganache.provider);
 
-console.log('test');
+
+beforeEach(() => {
+    console.log('test prep');
+    web3.eth.getAccounts()
+        .then(accounts => {
+            console.log(accounts);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+describe('Inbox',() => {
+    it('deploys a contract', () => {
+        assert(true, 'ok');
+    });
+} )
