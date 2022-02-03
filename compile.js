494c39983795fbb59ade78f466595cfd0df5ea5b
Albert Garuda <algarud@gmail.com>
2022-02-03 02:05:58 +0200

    Compiling changes

--- compile.js
+++ compile.js
@@ -2,53 +2,31 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
 
-var solcInput = {
-    language: "Solidity",
-    sources: { 
-        contract: {
+var input = {
+    language: 'Solidity',
+    sources: {
+        'inbox.sol' : {
             content: source
         }
-     },
+    },
     settings: {
-        optimizer: {
-            enabled: true
-        },
-        evmVersion: "byzantium",
         outputSelection: {
-            "*": {
-              "": [
-                "legacyAST",
-                "ast"
-              ],
-              "*": [
-                "abi",
-                "evm.bytecode.object",
-                "evm.bytecode.sourceMap",
-                "evm.deployedBytecode.object",
-                "evm.deployedBytecode.sourceMap",
-                "evm.gasEstimates"
-              ]
-            },
+            '*': {
+                '*': [ '*' ]
+            }
         }
     }
+}; 
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+const interface = output.contracts['inbox.sol'].Inbox.abi;
+const bytecode = output.contracts['inbox.sol'].Inbox.evm.bytecode.object;
+
+module.exports = {
+    interface,
+    bytecode,
 };
-
-solcInput = JSON.stringify(solcInput);
-
-result = solc.compile(solcInput);
-parsed = JSON.parse(result);
-//console.log(JSON.stringify(parsed.contracts.contract["Inbox"], null, 2));
-
-module.exports = parsed.contracts.contract["Inbox"]
-
-// fs.writeFile("test.txt", result, function(err) {
-//     if (err) {
-//         console.log(err);
-//     }
-// });
-
-// var jsonString = '{"some":"json"}';
-// console.log(JSON.stringify(JSON.parse(jsonString),null,2));
