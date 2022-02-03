8cce24b38eb9a3eb9009ba244882d182cbc1546a
Albert Garuda <algarud@gmail.com>
2022-02-03 02:06:06 +0200

    Succeeded to deploy contract from test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,6 +3,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile.js');
 
 //const web3 = new Web3(ganache.provider);
 
@@ -10,10 +11,17 @@ let accounts;
 beforeEach(async () => {
     console.log('test prep');
     accounts = await web3.eth.getAccounts();
+
+    console.log(interface);
+    // console.log(bytecode);
+
+    const contract = new web3.eth.Contract(interface);
+        
+    contract.deploy({ data: bytecode, arguments: ['hello there'] });
 });
 
-describe('Inbox',() => {
+describe('Inbox', () => {
     it('deploys a contract', () => {
         assert(true, 'ok');
     });
-} )
+})
